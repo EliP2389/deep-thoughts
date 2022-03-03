d167d0ff9b8fb8da9e96941612cbf5a360b04559
Eli Pagan <mbk8689@gmail.com>
2022-03-03 07:38:10 -0500

    connected fronnt and back end to display thoughts

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+
 import Home from './pages/Home';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
@@ -16,13 +23,13 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
     </ApolloProvider>
   );
 }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_THOUGHTS = gql`
-query thoughts($username: String) {
+  query thoughts($username: String) {
     thoughts(username: $username) {
       _id
       thoughtText
